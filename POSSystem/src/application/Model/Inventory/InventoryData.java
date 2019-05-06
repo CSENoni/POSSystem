2feea17e90282ffbd5ab714066fb508106e53dbc
ykravtsova <krav3025@stthomas.edu>
2019-05-05 21:48:22 -0500

    Inventory starting code

--- POSSystem/src/application/Model/Inventory/InventoryData.java
+++ POSSystem/src/application/Model/Inventory/InventoryData.java
@@ -1,5 +1,40 @@
 package application.Model.Inventory;
 
 public class InventoryData {
-
+	private int productId;
+	private String productName;
+	private int price;
+	private int quantity;
+	private List<Integer> productIds;
+	
+	public InventoryData(int productId, String productName, int price, int quantity) {
+		this.proudctId = productId;
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public getProductId() {
+		return productId;
+	}
+	
+	public getProductName() {
+		return productName;
+	}
+	
+	public getPrice() {
+		return price;
+	}
+	
+	public getQuantity() {
+		return quantity;
+	}
+	
+	public void addProduct() {
+		this.quantity++;
+	}
+	
+	public void removeProduct() {
+		this.quantity--;
+	}
 }
