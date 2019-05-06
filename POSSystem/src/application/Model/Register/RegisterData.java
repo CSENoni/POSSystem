b40d932194031482e4ddc21f00ffaa14325246ea
CSENoni <nonivhm@gmail.com>
2019-05-05 22:45:43 -0500

    Add file system operators for Register

--- POSSystem/src/application/Model/Register/RegisterData.java
+++ POSSystem/src/application/Model/Register/RegisterData.java
@@ -1,24 +1,38 @@
 package application.Model.Register;
 
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class RegisterData {
-	private int id;
-	private static AtomicInteger id_generator  = new AtomicInteger(0);
+public class RegisterData implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private String id;
 	private List<Integer> userIds;
 	private Date date;
 	private double amountOfSales;
+	private static AtomicInteger id_intgenerator = new AtomicInteger(0);
 	
-	public RegisterData(int id, int userId, Date date, double amountOfSales) {
-		this.id = id_generator.getAndIncrement();
+	public RegisterData(int userId, Date date, double amountOfSales) {
+		String id_generator = getUniqueId();
+		if(id_generator == null) {
+			id = "" + id_intgenerator.getAndIncrement();
+		}else {
+			id = id_generator;
+		}
+		userIds = new LinkedList<Integer>();
 		userIds.add(userId);
 		this.date = date;
 		this.amountOfSales = amountOfSales;
 	}
 	
-	public int getId() {
+	public String getId() {
 		return id;
 	}
 	
@@ -37,5 +51,27 @@ public class RegisterData {
 	}
 	public void setAmountOfSales(double amountOfSales) {
 		this.amountOfSales = amountOfSales;
+	}
+	
+	private static String getUniqueId() {
+ 		try {
+ 			InetAddress address = InetAddress.getLocalHost();
+ 		    NetworkInterface networkInterface = NetworkInterface.getByInetAddress(address);
+ 		    byte[] macAddress = networkInterface.getHardwareAddress();
+ 		    StringBuilder sb = new StringBuilder();
+ 		   for (int byteIndex = 0; byteIndex < macAddress.length; byteIndex++) {
+ 			    sb.append(String.format("%02X%s", macAddress[byteIndex], ""));
+ 			}
+ 			return sb.toString();
+ 		} catch (SocketException | UnknownHostException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
+ 		return null;
+ 	}
+	
+	@Override
+	public String toString() {
+		return "Register: " + this.id + " Amount: " + this.amountOfSales;
 	}
 }
--- POSSystem/src/application/Model/Register/RegisterUtils.java
+++ POSSystem/src/application/Model/Register/RegisterUtils.java
@@ -1,5 +1,54 @@
 package application.Model.Register;
 
-public class RegisterUtils {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
+public class RegisterUtils {
+	public static void write(RegisterData data) {
+		try {
+			List<RegisterData> datas = getAll();
+			
+			FileOutputStream fs = new FileOutputStream(new File("example.txt"));
+			ObjectOutputStream o = new ObjectOutputStream(fs);
+			
+			if(datas == null) {
+				List<RegisterData> d = new ArrayList<RegisterData>();
+				d.add(data);
+				o.writeObject(d);
+			}else {
+				datas.add(data);
+				o.writeObject(datas);
+			}
+			
+			o.close();
+			fs.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static List<RegisterData> getAll() {
+		try {
+			File file = new File("example.txt");
+			FileInputStream fs = null;
+			ObjectInputStream oi = null;
+			if(file.exists()) {
+				fs = new FileInputStream(file);
+			    oi = new ObjectInputStream(fs);
+			    List<RegisterData> datas = (List<RegisterData>) oi.readObject();
+			    oi.close();
+				fs.close();
+				return datas;
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
