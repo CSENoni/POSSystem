b02641ac193b0ed0b1bc40921cc784cf553df57f
CSENoni <nonivhm@gmail.com>
2019-05-06 21:48:11 -0500

    Implement User Login logic

--- POSSystem/src/application/controller/LoginController.java
+++ POSSystem/src/application/controller/LoginController.java
@@ -3,9 +3,12 @@ package application.controller;
 import java.io.IOException;
 
 import application.Model.POSUtils;
+import application.Model.User.UserUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 
 public class LoginController {
 	
@@ -15,8 +18,40 @@ public class LoginController {
 	@FXML
 	private TextField userPassword;
 	
+	@FXML
+	private Text errorInput;
+	
+	@FXML
+	private void initialize() {
+		// This is just for test. Remove it before presentation
+		if(userName != null && userPassword != null) {
+			userName.setText("tester1");
+			userPassword.setText("12345");
+		}
+	}
 	
 	public void loginToHomeScreen(ActionEvent event) throws IOException {
-		POSUtils.changeScene(event, getClass(), "../view/Home.fxml");
+		String name = userName.getText();
+		String pass = userPassword.getText();
+		
+		if(isNotEmpty(name) && isNotEmpty(pass) && UserUtils.get(name, pass)) { 
+			POSUtils.changeScene(event, getClass(), "../view/Home.fxml");
+		}else {
+			userName.setStyle("-fx-border-width: 3,3,3,3; -fx-border-color: red;");
+			userPassword.setStyle("-fx-border-width: 3,3,3,3; -fx-border-color: red;");
+			errorInput.setVisible(true);
+		}
+	}
+	
+	public void fieldFilling(KeyEvent event) {
+		if(userName != null && userPassword != null) {
+			userName.setStyle("");
+			userPassword.setStyle("");
+			errorInput.setVisible(false);
+		}
+	}
+	
+	private boolean isNotEmpty(String text) {
+		return text != null && text.length() > 0;
 	}
 }
