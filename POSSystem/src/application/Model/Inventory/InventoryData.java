807f48498923c75b1ba18976cbaa810d5a3a882b
CSENoni <nonivhm@gmail.com>
2019-05-09 20:39:01 -0500

    Update file system for Inventory (not complete)

--- POSSystem/src/application/Model/Inventory/InventoryData.java
+++ POSSystem/src/application/Model/Inventory/InventoryData.java
@@ -12,14 +12,16 @@ public class InventoryData implements Serializable {
 	private double price;
 	private int stockQuantity; //the number of items in stock at the store
 	private int outstandingOrder; // number of quantity in pending orders
+	private int threshold;
 	private static AtomicInteger id_generator = new AtomicInteger(0);
 	
-	public InventoryData(String productName, String supplier, double price, int quantity) {
+	public InventoryData(String productName, String supplier, double price, int quantity, int threshold) {
 		this.productId = id_generator.getAndIncrement();
 		this.productName = productName;
 		this.price = price;
 		this.supplier = supplier;
 		this.stockQuantity = quantity;
+		this.threshold = threshold;
 	}
 	
 	public int getProductId() {
@@ -60,5 +62,9 @@ public class InventoryData implements Serializable {
 	
 	public void removeProduct(int quantity) { //after a customer makes a purchase
 		this.stockQuantity -= quantity;
+	}
+	
+	public int getThreshold() {
+		return this.threshold;
 	}
 }
--- POSSystem/src/application/Model/Inventory/InventoryUtils.java
+++ POSSystem/src/application/Model/Inventory/InventoryUtils.java
@@ -1,7 +1,73 @@
 package application.Model.Inventory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryUtils {
-	public static void write() {
+	public static void write(InventoryData data) {
+		try {
+			List<InventoryData> datas = getAll();
+
+			FileOutputStream fs = new FileOutputStream(new File("Inventory.txt"));
+			ObjectOutputStream o = new ObjectOutputStream(fs);
+
+			if (datas == null) {
+				List<InventoryData> d = new ArrayList<InventoryData>();
+				d.add(data);
+				o.writeObject(d);
+			} else {
+				datas.add(data);
+				o.writeObject(datas);
+			}
+
+			o.close();
+			fs.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static List<InventoryData> getAll() {
+		try {
+			File file = new File("Inventory.txt");
+			FileInputStream fs = null;
+			ObjectInputStream oi = null;
+			if (file.exists()) {
+				fs = new FileInputStream(file);
+				oi = new ObjectInputStream(fs);
+				List<InventoryData> datas = (List<InventoryData>) oi.readObject();
+				oi.close();
+				fs.close();
+				return datas;
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void remove(int position) {
+		List<InventoryData> datas = getAll();
 		
+		if(datas != null) {
+			datas.remove(position);
+			try {
+				FileOutputStream fs = new FileOutputStream(new File("Inventory.txt"));
+				ObjectOutputStream o = new ObjectOutputStream(fs);
+				
+				o.writeObject(datas);
+				o.close();
+				fs.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
