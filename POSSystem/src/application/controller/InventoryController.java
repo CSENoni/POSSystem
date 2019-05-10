8dde5b48fd9a5a19fe43a50e037f753a9a7779cd
CSENoni <nonivhm@gmail.com>
2019-05-10 10:30:53 -0500

    Update Inventory logic (not complete)

--- POSSystem/src/application/controller/InventoryController.java
+++ POSSystem/src/application/controller/InventoryController.java
@@ -1,7 +1,14 @@
 package application.controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class InventoryController {
 	@FXML
@@ -13,7 +20,16 @@ public class InventoryController {
 	}
 	
 	public void addNewItem(ActionEvent event) {
-		
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/InventoryDialog.fxml"));
+			Parent root = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void editItem(ActionEvent event) {
--- POSSystem/src/application/controller/InventoryDialogController.java
+++ POSSystem/src/application/controller/InventoryDialogController.java
@@ -1,14 +1,37 @@
 package application.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class InventoryDialogController {
+	
+	@FXML
+	private Button cancelButton;
+	
+	@FXML
+	private TextField productField;
+	
+	@FXML
+	private TextField supplierField;
+	
+	@FXML
+	private TextField quantityField;
+	
+	@FXML
+	private TextField pendingField;
+	
+	@FXML
+	private TextField thresholdField;
 	
 	public void confirmItemDetail(ActionEvent event) {
 		
 	}
 	
 	public void cancelItemDetail(ActionEvent event) {
-		
+		Stage stage = (Stage) cancelButton.getScene().getWindow();
+		stage.close();
 	}
 }
