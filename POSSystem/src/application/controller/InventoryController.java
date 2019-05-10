432bd2580427ff5e00d8370016b82f37e004a79a
CSENoni <nonivhm@gmail.com>
2019-05-10 15:56:03 -0500

    Update inventory logic (not complete)

--- POSSystem/src/application/controller/InventoryController.java
+++ POSSystem/src/application/controller/InventoryController.java
@@ -1,12 +1,20 @@
 package application.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import application.Model.Inventory.InventoryData;
+import application.Model.Inventory.InventoryUtils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -15,14 +23,54 @@ public class InventoryController {
 	private HeaderController headerViewController;
 	
 	@FXML
+	private TableView<InventoryData> tableData;
+	
+	@FXML
+	private TableColumn<InventoryData, Integer> numberColumn;
+	
+	@FXML
+	private TableColumn<InventoryData, String> productColumn;
+	
+	@FXML
+	private TableColumn<InventoryData, String> supplierColumn;
+	
+	@FXML
+	private TableColumn<InventoryData, Integer> quantityColumn;
+	
+	@FXML
+	private TableColumn<InventoryData, Integer> pendingColumn;
+	
+	@FXML
+	private TableColumn<InventoryData, Integer> thresholdColumn;
+	
+	private ObservableList<InventoryData> inventoryData;
+	
+	@FXML
 	private void initialize() {
 		headerViewController.setTitle("INVENTORY");
+		inventoryData = FXCollections.observableArrayList();
+		
+		List<InventoryData> list = InventoryUtils.getAll();
+		if(list != null)
+			inventoryData.addAll(list);
+		
+//		numberColumn.setCellValueFactory(new PropertyValueFactory<>("ProductId"));
+		productColumn.setCellValueFactory(new PropertyValueFactory<>("ProductName"));
+		supplierColumn.setCellValueFactory(new PropertyValueFactory<>("Supplier"));
+		quantityColumn.setCellValueFactory(new PropertyValueFactory<>("StockQuantity"));
+		pendingColumn.setCellValueFactory(new PropertyValueFactory<>("OutstandingOrder"));
+		thresholdColumn.setCellValueFactory(new PropertyValueFactory<>("Threshold"));
+		
+		tableData.setItems(inventoryData);
 	}
 	
 	public void addNewItem(ActionEvent event) {
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/InventoryDialog.fxml"));
 			Parent root = (Parent) fxmlLoader.load();
+			InventoryDialogController dialogController = fxmlLoader.getController();
+			dialogController.setInventoryData(inventoryData);
+			
 			Stage stage = new Stage();
 			stage.setScene(new Scene(root));
 			stage.initModality(Modality.APPLICATION_MODAL);
--- POSSystem/src/application/controller/InventoryDialogController.java
+++ POSSystem/src/application/controller/InventoryDialogController.java
@@ -1,5 +1,8 @@
 package application.controller;
 
+import application.Model.Inventory.InventoryData;
+import application.Model.Inventory.InventoryUtils;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -21,16 +24,28 @@ public class InventoryDialogController {
 	private TextField quantityField;
 	
 	@FXML
-	private TextField pendingField;
-	
-	@FXML
 	private TextField thresholdField;
 	
-	public void confirmItemDetail(ActionEvent event) {
-		
+	private ObservableList<InventoryData> data;
+	
+	public void setInventoryData(ObservableList<InventoryData> data) {
+		this.data = data;
 	}
 	
-	public void cancelItemDetail(ActionEvent event) {
+	public void confirmItemDetail(ActionEvent event) {
+		String productName = productField.getText();
+		String supplier = supplierField.getText();
+		int quantity = Integer.parseInt(quantityField.getText());
+		int threshold = Integer.parseInt(thresholdField.getText());
+		
+		InventoryData newInventoryData = new InventoryData(productName, supplier, 1200, quantity, threshold);
+		InventoryUtils.write(newInventoryData);
+		
+		data.add(newInventoryData);
+		closeItemDetail(event);
+	}
+	
+	public void closeItemDetail(ActionEvent event) {
 		Stage stage = (Stage) cancelButton.getScene().getWindow();
 		stage.close();
 	}
