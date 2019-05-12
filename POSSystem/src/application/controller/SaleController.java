cbc3b7f6566c84313f15c14c1f73cbf46bd56f1e
CSENoni <nonivhm@gmail.com>
2019-05-12 18:09:51 -0500

    Add access to Sale Add Item UI

--- POSSystem/src/application/controller/SaleController.java
+++ POSSystem/src/application/controller/SaleController.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 import application.Model.POSUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class SaleController {
 	
@@ -13,8 +18,26 @@ public class SaleController {
 	private HeaderController headerViewController;
 	
 	@FXML
+	private Text saleName;
+	
+	@FXML
 	private void initialize() {
 		headerViewController.setTitle("SALE");
+	}
+	
+	public void addAndEditItem(ActionEvent event) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/SaleAddItem.fxml"));
+			Parent root = (Parent) fxmlLoader.load();
+//			SaleAddItemController dialogController = fxmlLoader.getController();
+
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void toCheckOut(ActionEvent event) throws IOException {
