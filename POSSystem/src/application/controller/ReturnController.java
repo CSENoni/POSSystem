bcea3425b79b6db2145577f591f177a94eccfac5
ykravtsova <krav3025@stthomas.edu>
2019-05-14 20:05:29 -0500

    Return screen similar to Sale

--- POSSystem/src/application/controller/ReturnController.java
+++ POSSystem/src/application/controller/ReturnController.java
@@ -5,10 +5,17 @@ package application.controller;
 import java.io.IOException;
 
 import application.Model.POSUtils;
+import application.Model.Inventory.InventoryData;
 import application.Model.Sale.SaleData;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class ReturnController {
 
@@ -26,7 +33,24 @@ public class ReturnController {
 				
 	}
 	
+	@FXML
+	private ObservableList<InventoryData> inventorySaleList;
 	
+	public void addAndEditItem(ActionEvent event) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/SaleAddItem.fxml"));
+			Parent root = (Parent) fxmlLoader.load();
+			SaleAddItemController dialogController = fxmlLoader.getController();
+			dialogController.setSaleTable(inventorySaleList);
+			
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public void toCompleteReturn(ActionEvent event) throws IOException {
 		POSUtils.changeScene(event, getClass(), "../view/ReturnComplete.fxml");
